39b5baaa5cf5d78eb733f98117b2cc726c48b1c0
j2nhyeok <103464891+j2nhyeok@users.noreply.github.com>
2022-07-29 17:48:19 +0900

    Add files via upload

---
+++ userPlaylist/PerPlayList.java
@@ -0,0 +1,13 @@
+package userPlaylist;
+
+public class PerPlayList {
+	private String userID;
+	private String title;
+	private String artist;
+	private String genre;
+	private int nowRank;
+	
+	public PerPlayList() {
+		
+	}
+}
